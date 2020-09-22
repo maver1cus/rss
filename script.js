a44a3cbdd5166132530221a63a81b080d98e75f5
Sergey Petrov <mavericusdev@gmail.com>
2020-09-22 17:11:08 +0300

    implements basic calculator functionality and work with negative numbers

--- script.js
+++ script.js
@@ -1,58 +1,140 @@
-const memoryOperandPendingOperation = '';
-const currentOperand = 0;
-const currentOperation = '';
-const calculatorGrid = document.querySelector('.calculator-grid');
-const outputDisplayCurrentOperand = document.querySelector('.current-operand');
-const outputDisplayPreviousOperand = document.querySelector('.previous-operand');
+let fitstOperand = ''
+let currentOperand = '0'
+let currentOperation = ''
+const calculatorGrid = document.querySelector('.calculator-grid')
+const mainDisplay = document.querySelector('.current-operand')
+const secondDisplay = document.querySelector('.previous-operand')
+
+const showToMainDisplay = str => mainDisplay.textContent = str
+const showToSecondDisplay = str => secondDisplay.textContent = str
 
 const inputNumber = (number) => {
-  console.log('Input number: ', number)
+  currentOperand = (currentOperand.toString() === '0')
+    ? number
+    : currentOperand.toString() + number.toString()
+  showToMainDisplay(currentOperand)
 }
 
 const choiceOperation = (operationName) => {
-  console.log('Choise operation: ', operationName)
+  if (operationName === '-' && currentOperand === '0') {
+    currentOperand = '-'
+    showToMainDisplay(currentOperand)
+  } else if (fitstOperand && currentOperation) {
+    currentOperation = operationName
+    fitstOperand = calculate()
+    currentOperand = '0'
+    showToMainDisplay(currentOperand.toString())
+    showToSecondDisplay(`${fitstOperand} ${currentOperation}`)
+  } else {
+    currentOperation = operationName
+    fitstOperand = currentOperand
+    currentOperand = '0'
+    showToSecondDisplay(`${fitstOperand} ${currentOperation}`)
+    showToMainDisplay(currentOperand)
+  }
 }
 
 const clear = () => {
-  console.log('clear')
+  memoryOperandPendingOperation = ''
+  currentOperand = '0'
+  currentOperation = ''
+  showToMainDisplay(currentOperand)
+  showToSecondDisplay('')
 }
 
 const calculate = () => {
-  console.log('calculate')
+  if (fitstOperand === '' || currentOperand === '0' || currentOperation === '') {
+    return false
+  }
+  let result
+  switch(currentOperation) {
+    case '-':
+      result = Number(fitstOperand) - Number(currentOperand)
+      break
+    case '/':
+      result = Number(fitstOperand) / Number(currentOperand)
+      break
+    case '*':
+      result = Number(fitstOperand) * Number(currentOperand)
+      break
+    case '+':
+      result = Number(fitstOperand) + Number(currentOperand)
+      break
+    default:
+      break
+  }
+
+  return result
 }
 
 const deleteNumber = () => {
-  console.log('delete')
+  currentOperand = currentOperand.length === 1
+    ? '0'
+    : currentOperand.substring(0, currentOperand.length - 1)
+  showToMainDisplay(currentOperand)
 }
 
 const inputPoint = () => {
-  console.log('input point')
+  if (!currentOperand.toString().includes('.')) {
+    currentOperand = currentOperand.toString() + '.'
+    showToMainDisplay(currentOperand)
+  }
 }
+
+const equals = () => {
+  if (fitstOperand && currentOperation && currentOperand !== '0') {
+    currentOperand = calculate()
+    fitstOperand = ''
+    currentOperation = ''
+    showToMainDisplay(currentOperand.toString())
+    showToSecondDisplay('')
+  }
+}
+
+showToMainDisplay(currentOperand)
 
 calculatorGrid.addEventListener('click', (evt) => {
   const btnType = evt.target.dataset.type
   const btnValue = evt.target.dataset.value
 
-  switch (btnType) {
+  switch(btnType) {
     case 'number':
       inputNumber(btnValue)
-      break;
+      break
     case 'operation':
       choiceOperation(btnValue)
-      break;
+      break
     case 'equals':
-      calculate ()
-      break;
+      equals()
+      break
     case 'all-clear':
       clear()
-      break;
+      break
     case 'delete':
       deleteNumber()
-      break;
+      break
     case 'point':
       inputPoint()
-      break;
+      break
     default:
-      return;
+      return
+  }
+})
+
+document.addEventListener('keydown', function(event) {
+  if (event.key >= 0 && event.key <= 9) {
+    inputNumber(event.key)
+  }
+  if (event.key.toLowerCase() === 'enter' || event.key === '=') {
+    equals()
+  }
+  if (['+', '-', '/', '*'].includes(event.key)) {
+    choiceOperation(event.key)
+  }
+  if (event.key.toLowerCase() === 'delete') {
+    deleteNumber()
+  }
+  if (event.key === '.') {
+    inputPoint()
   }
 })
