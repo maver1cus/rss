cd2195dc684e5efb5293867729de0ce142fade4c
Sergey <mavericusdev@gmail.com>
2020-10-26 22:32:22 +0300

    feat: redeploy shelter

---
+++ shelter/pages/main/app.js
@@ -0,0 +1,258 @@
+const PETS = [
+  {
+    "name": "Jennifer",
+    "img": "../../assets/images/jennifer.jpg",
+    "type": "Dog",
+    "breed": "Labrador",
+    "description": "Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
+    "age": "2 months",
+    "inoculations": ["none"],
+    "diseases": ["none"],
+    "parasites": ["none"]
+  },
+  {
+    "name": "Sophia",
+    "img": "../../assets/images/sophia.jpg",
+    "type": "Dog",
+    "breed": "Shih tzu",
+    "description": "Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
+    "age": "1 month",
+    "inoculations": ["parvovirus"],
+    "diseases": ["none"],
+    "parasites": ["none"]
+  },
+  {
+    "name": "Woody",
+    "img": "../../assets/images/woody.jpg",
+    "type": "Dog",
+    "breed": "Golden Retriever",
+    "description": "Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.",
+    "age": "3 years 6 months",
+    "inoculations": ["adenovirus", "distemper"],
+    "diseases": ["right back leg mobility reduced"],
+    "parasites": ["none"]
+  },
+  {
+    "name": "Scarlett",
+    "img": "../../assets/images/scarlett.jpg",
+    "type": "Dog",
+    "breed": "Jack Russell Terrier",
+    "description": "Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.",
+    "age": "3 months",
+    "inoculations": ["parainfluenza"],
+    "diseases": ["none"],
+    "parasites": ["none"]
+  },
+  {
+    "name": "Katrine",
+    "img": "../../assets/images/katrine.jpg",
+    "type": "Cat",
+    "breed": "British Shorthair",
+    "description": "Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.",
+    "age": "6 months",
+    "inoculations": ["panleukopenia"],
+    "diseases": ["none"],
+    "parasites": ["none"]
+  },
+  {
+    "name": "Timmy",
+    "img": "../../assets/images/timmy.jpg",
+    "type": "Cat",
+    "breed": "British Shorthair",
+    "description": "Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.",
+    "age": "2 years 3 months",
+    "inoculations": ["calicivirus", "viral rhinotracheitis"],
+    "diseases": ["kidney stones"],
+    "parasites": ["none"]
+  },
+  {
+    "name": "Freddie",
+    "img": "../../assets/images/freddie.jpg",
+    "type": "Cat",
+    "breed": "British Shorthair",
+    "description": "Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.",
+    "age": "2 months",
+    "inoculations": ["rabies"],
+    "diseases": ["none"],
+    "parasites": ["none"]
+  },
+  {
+    "name": "Charly",
+    "img": "../../assets/images/charly.jpg",
+    "type": "Dog",
+    "breed": "Jack Russell Terrier",
+    "description": "This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
+    "age": "8 years",
+    "inoculations": ["bordetella bronchiseptica", "leptospirosis"],
+    "diseases": ["deafness", "blindness"],
+    "parasites": ["lice", "fleas"]
+  }
+];
+const bodyElement = document.querySelector('body');
+const sliderWrapper = document.querySelector('.slider__wrapper');
+const mainMenu = document.querySelector('.main-menu');
+const mainMenuBtn = document.querySelector('.main-menu__btn')
+const mainMenuDisabledLinks = document.querySelectorAll('.main-menu__link--disabled');
+const mainMenuOverlay = document.querySelector('.main-menu__overlay');
+
+let isEnabled = true;
+
+mainMenuDisabledLinks.forEach(link => link.addEventListener('click', evt => evt.preventDefault()));
+
+const getCountCardsInSliderItem = () => {
+  const widthScreen = document.documentElement.clientWidth;
+  let countCardsInSliderItem = 3;
+  if (widthScreen < 767) {
+    countCardsInSliderItem = 1;
+  } else if ( widthScreen < 1279 ) {
+    countCardsInSliderItem = 2;
+  }
+  return countCardsInSliderItem;
+}
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const getPopupTemplate = ({name, img, breed, description, age, inoculations, diseases, parasites}) => (`
+  <div class="popup__content">
+    <div class="popup__image">
+      <img src="${img}" alt="${name}">
+    </div>
+    <div class="popup__note">
+      <div class="popup__name">${name}</div>
+      <div class="popup__breed">${breed}</div>
+      <p class="popup__description">${description}</p>
+      <ul class="popup__feature">
+        <li class="popup__feature-item"><b>Age:</b> ${age}</li>
+        <li class="popup__feature-item"><b>Inoculations:</b> ${inoculations.join(' ')}</li>
+        <li class="popup__feature-item"><b>Diseases:</b> ${diseases.join(' ')}</li>
+        <li class="popup__feature-item"><b>Parasites:</b> ${parasites.join(' ')}</li>
+      </ul>
+      <button class="btn btn--secondary popup__close-btn">
+        <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg" class="popup__close-btn-svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M7.42618 6.00003L11.7046 1.72158C12.0985 1.32775 12.0985 0.689213 11.7046 0.295433C11.3108 -0.0984027 10.6723 -0.0984027 10.2785 0.295433L5.99998 4.57394L1.72148 0.295377C1.32765 -0.098459 0.68917 -0.098459 0.295334 0.295377C-0.0984448 0.689213 -0.0984448 1.32775 0.295334 1.72153L4.57383 5.99997L0.295334 10.2785C-0.0984448 10.6723 -0.0984448 11.3108 0.295334 11.7046C0.68917 12.0985 1.32765 12.0985 1.72148 11.7046L5.99998 7.42612L10.2785 11.7046C10.6723 12.0985 11.3108 12.0985 11.7046 11.7046C12.0985 11.3108 12.0985 10.6723 11.7046 10.2785L7.42618 6.00003Z" fill="#292929"/>
+        </svg>
+      </button>
+    </div>
+  </div>`
+  );
+
+const closePopup = (evt) => {
+  const target = evt.target;
+  if (target.classList.contains('popup') || target.classList.contains('popup__close-btn') || target.classList.contains('popup__close-btn-svg')) {
+    document.querySelector('.popup').remove();
+    bodyElement.classList.remove('lock');
+  }
+}
+
+const createPopup = card => {
+  const popupElement = document.createElement('div');
+  popupElement.classList.add('popup');
+  popupElement.insertAdjacentHTML('beforeend', getPopupTemplate(card));
+  popupElement.addEventListener('click', closePopup);
+  return popupElement;
+}
+
+const openPopup = evt => {
+  const namePet = evt.currentTarget.dataset.pet;
+  const pet = PETS.find(pet => pet.name === namePet);
+  const outElement = document.querySelector('.out');
+  outElement.insertAdjacentElement('beforeend', createPopup(pet) );
+  bodyElement.classList.add('lock')
+
+}
+
+const getCardTemplate = ({name, img}) => (
+  `<div class="card__image">
+    <img src="${img}" alt="${name}">
+  </div>
+  <div class="card__description">
+    <div class="card__title">${name}</div>
+    <buttton class="slider__btn btn btn--secondary">Learn more</buttton>
+  </div>`);
+
+const getCard = card => {
+  const fragment = document.createElement('div');
+  fragment.classList.add('card')
+  fragment.dataset.pet = card.name;
+  fragment.insertAdjacentHTML('beforeend', getCardTemplate(card));
+  fragment.addEventListener('click', openPopup)
+  return fragment;
+}
+
+const createSliderItem = (classItem) => {
+  const sliderItem = document.createElement('div');
+  const countCards = getCountCardsInSliderItem();
+  sliderItem.classList.add('slider__item')
+  sliderItem.classList.add(classItem);
+  const cards = new Set();
+
+  while(cards.size < countCards) {
+    cards.add(PETS[getRandomInt(0, PETS.length)]);
+  }
+  cards.forEach(card => sliderItem.insertAdjacentElement('beforeend', getCard(card)));
+  return sliderItem;
+}
+
+const hideItem = direction => {
+  isEnabled = false;
+  const hideSliderItem = document.querySelector('.slider__item--active');
+  hideSliderItem.classList.add(direction);
+  hideSliderItem.addEventListener('animationend', function() {
+    this.remove();
+  });
+}
+
+const showItem = direction => {
+  const nextSliderItem = document.querySelector('.slider__item--next')
+  nextSliderItem.classList.add(direction);
+  nextSliderItem.addEventListener('animationend', function() {
+    this.classList.remove('slider__item--next', direction);
+    this.classList.add('slider__item--active');
+    isEnabled = true;
+  });
+}
+
+const nextItem = () => {
+  hideItem('to-left');
+  showItem('from-right');
+}
+
+const previousItem = () => {
+  hideItem('to-right');
+  showItem('from-left');
+}
+
+document.querySelector('.slider__nav-btn--prev').addEventListener('click', function() {
+  if (isEnabled) {
+    sliderWrapper.insertAdjacentElement('afterbegin', createSliderItem('slider__item--next'));
+    previousItem();
+  }
+});
+
+document.querySelector('.slider__nav-btn--next').addEventListener('click', function() {
+  if (isEnabled) {
+    sliderWrapper.insertAdjacentElement('beforeend', createSliderItem('slider__item--next'));
+    nextItem();
+  }
+});
+
+sliderWrapper.innerHTML = '';
+sliderWrapper.insertAdjacentElement('beforeend', createSliderItem('slider__item--active'));
+
+mainMenuBtn.addEventListener('click', evt => {
+  mainMenu.classList.toggle('main-menu--open')
+  evt.stopPropagation()
+});
+
+mainMenuOverlay.addEventListener('click', evt => {
+  if (evt.target.classList.contains('main-menu__overlay')) {
+    mainMenu.classList.remove('main-menu--open');
+  }
+})
+
+
+
---
+++ shelter/pages/pets/js/app.js
@@ -0,0 +1,242 @@
+const page = document.querySelector(".cards__list");
+const numPage = document.querySelector(".cards__count");
+const btnPrev = document.querySelector('.cards__btn--prev');
+const btnNext = document.querySelector('.cards__btn--next');
+const btnFirst = document.querySelector('.cards__btn--first')
+const btnLast = document.querySelector('.cards__btn--last')
+const bodyElement = document.querySelector('body');
+const mainMenu = document.querySelector('.main-menu');
+const mainMenuBtn = document.querySelector('.main-menu__btn');
+const mainMenuDisabledLinks = document.querySelectorAll('.main-menu__link--disabled');
+const mainMenuOverlay = document.querySelector('.main-menu__overlay');
+
+mainMenuDisabledLinks.forEach(link => link.addEventListener('click', evt => evt.preventDefault()));
+
+fetch("../../assets/pets.json")
+  .then((res) => res.json())
+  .then((pets) => {
+
+    const getPopupTemplate = ({name, img, breed, description, age, inoculations, diseases, parasites}) => (
+      `<div class="popup__content">
+        <div class="popup__image">
+          <img src="${img}" alt="${name}">
+        </div>
+        <div class="popup__note">
+          <div class="popup__name">${name}</div>
+          <div class="popup__breed">${breed}</div>
+          <p class="popup__description">${description}</p>
+          <ul class="popup__feature">
+            <li class="popup__feature-item"><b>Age:</b> ${age}</li>
+            <li class="popup__feature-item"><b>Inoculations:</b> ${inoculations.join(' ')}</li>
+            <li class="popup__feature-item"><b>Diseases:</b> ${diseases.join(' ')}</li>
+            <li class="popup__feature-item"><b>Parasites:</b> ${parasites.join(' ')}</li>
+          </ul>
+          <button class="btn btn--secondary popup__close-btn">
+            <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg" class="popup__close-btn-svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M7.42618 6.00003L11.7046 1.72158C12.0985 1.32775 12.0985 0.689213 11.7046 0.295433C11.3108 -0.0984027 10.6723 -0.0984027 10.2785 0.295433L5.99998 4.57394L1.72148 0.295377C1.32765 -0.098459 0.68917 -0.098459 0.295334 0.295377C-0.0984448 0.689213 -0.0984448 1.32775 0.295334 1.72153L4.57383 5.99997L0.295334 10.2785C-0.0984448 10.6723 -0.0984448 11.3108 0.295334 11.7046C0.68917 12.0985 1.32765 12.0985 1.72148 11.7046L5.99998 7.42612L10.2785 11.7046C10.6723 12.0985 11.3108 12.0985 11.7046 11.7046C12.0985 11.3108 12.0985 10.6723 11.7046 10.2785L7.42618 6.00003Z" fill="#292929"/>
+            </svg>
+          </button>
+        </div>
+      </div>`
+      );
+
+    const createPopup = card => {
+      const popupElement = document.createElement('div');
+      popupElement.classList.add('popup');
+      popupElement.insertAdjacentHTML('beforeend', getPopupTemplate(card));
+      popupElement.addEventListener('click', closePopup);
+      return popupElement;
+    }
+
+    const openPopup = evt => {
+      console.log(fullPetsList);
+      console.log(12);
+      const namePet = evt.currentTarget.dataset.pet;
+      const pet = fullPetsList.find(pet => pet.name === namePet);
+      const outElement = document.querySelector('.out');
+      outElement.insertAdjacentElement('beforeend', createPopup(pet) );
+      bodyElement.classList.add('lock')
+
+    }
+
+    const closePopup = (evt) => {
+      const target = evt.target;
+      if (target.classList.contains('popup') || target.classList.contains('popup__close-btn') || target.classList.contains('popup__close-btn-svg')) {
+        document.querySelector('.popup').remove();
+        bodyElement.classList.remove('lock');
+      }
+    }
+
+    const getCardTemplate = ({name, img}) => (
+      `<div class="card">
+        <div class="card__image">
+          <img src="${img}" alt="${name}">
+        </div>
+        <div class="card__description">
+          <div class="card__title">${name}</div>
+          <buttton class="btn btn--secondary">Learn more</buttton>
+        </div>
+      </div>`
+    );
+
+    const getCard = card => {
+      const fragment = document.createElement('li');
+      fragment.classList.add('cards__item')
+      fragment.dataset.pet = card.name;
+      fragment.insertAdjacentHTML('beforeend', getCardTemplate(card));
+      fragment.addEventListener('click', openPopup)
+      return fragment;
+    }
+
+
+    const getStatePagination = (array) => {
+      let countCardPage = 8;
+      if (
+        document.querySelector("body").offsetWidth >= 768 &&
+        document.querySelector("body").offsetWidth < 1280
+      ) {
+        countCardPage = 6;
+      } else if (document.querySelector("body").offsetWidth < 768) {
+        countCardPage = 3;
+      }
+      return {
+        countCardPage: countCardPage,
+        countPages: array.length / countCardPage,
+        currentPage: 0,
+      };
+    };
+
+    const sort863 = (pets) => {
+      let list = JSON.parse(JSON.stringify(pets));
+      let unique8List = [];
+      let length = list.length;
+      for (let i = 0; i < length / 8; i++) {
+        const uniqueStepList = [];
+        for (j = 0; j < list.length; j++) {
+          if (uniqueStepList.length >= 8) {
+            break;
+          }
+          const isUnique = !uniqueStepList.some((item) => {
+            return item.name === list[j].name;
+          });
+          if (isUnique) {
+            uniqueStepList.push(list[j]);
+            list.splice(j, 1);
+            j--;
+          }
+        }
+        unique8List = [...unique8List, ...uniqueStepList];
+      }
+      list = unique8List;
+
+      list = sort6recursively(list);
+
+      return list;
+    };
+
+    const sort6recursively = (list) => {
+      const length = list.length;
+
+      for (let i = 0; i < length / 6; i++) {
+        const stepList = list.slice(i * 6, i * 6 + 6);
+
+        for (let j = 0; j < 6; j++) {
+          const duplicatedItem = stepList.find((item, ind) => {
+            return item.name === stepList[j].name && ind !== j;
+          });
+
+          if (duplicatedItem !== undefined) {
+            const ind = i * 6 + j;
+            const which8OfList = Math.trunc(ind / 8);
+
+            list.splice(which8OfList * 8, 0, list.splice(ind, 1)[0]);
+
+            sort6recursively(list);
+          }
+        }
+      }
+
+      return list;
+    };
+
+    const renderPage = (currentPage, fullPetsList, countCardPage) => {
+      const start = currentPage * countCardPage;
+      const end = currentPage * countCardPage + countCardPage;
+
+      const petsCurentPage = fullPetsList.slice(start, end);
+      page.innerHTML = ''
+      html = "";
+
+      petsCurentPage.forEach(card => page.insertAdjacentElement('beforeend', getCard(card)));
+      numPage.innerText = currentPage + 1;
+    };
+
+    const blockBtn = (currentPage, countPages) => {
+      if (currentPage === countPages - 1) {
+        btnNext.setAttribute("disabled", true);
+        btnLast.setAttribute("disabled", true);
+      } else {
+        btnNext.removeAttribute("disabled");
+        btnLast.removeAttribute("disabled");
+      }
+
+      if (currentPage <= 0) {
+        btnPrev.setAttribute("disabled", true);
+        btnFirst.setAttribute("disabled", true);
+      } else {
+        btnPrev.removeAttribute("disabled", true);
+        btnFirst.removeAttribute("disabled", true);
+      }
+    };
+
+    let list = [];
+    for (let i = 0; i < 6; i++) {
+      list = [...list, ...pets.sort(() => Math.random() - 0.5)];
+    }
+    let fullPetsList = sort863(list);
+
+    let { countCardPage, countPages, currentPage } = getStatePagination(
+      fullPetsList
+    );
+    renderPage(currentPage, fullPetsList, countCardPage);
+    blockBtn(currentPage, countPages);
+
+    btnNext.addEventListener('click', () => {
+      currentPage += 1;
+      renderPage(currentPage, fullPetsList, countCardPage);
+      blockBtn(currentPage, countPages);
+    });
+
+    btnPrev.addEventListener('click', () => {
+      currentPage -= 1;
+      renderPage(currentPage, fullPetsList, countCardPage);
+
+      blockBtn(currentPage, countPages);
+    });
+
+    btnFirst.addEventListener('click', () => {
+      currentPage = 0;
+      renderPage(currentPage, fullPetsList, countCardPage);
+
+      blockBtn(currentPage, countPages);
+    })
+
+
+    btnLast.addEventListener('click', () => {
+      currentPage = countPages - 1;
+      renderPage(currentPage, fullPetsList, countCardPage);
+
+      blockBtn(currentPage, countPages);
+    })
+  });
+
+mainMenuBtn.addEventListener('click', evt => {
+  mainMenu.classList.toggle('main-menu--open')
+  evt.stopPropagation()
+});
+
+mainMenuOverlay.addEventListener('click', evt => {
+  if (evt.target.classList.contains('main-menu__overlay')) {
+    mainMenu.classList.remove('main-menu--open');
+  }
+})
