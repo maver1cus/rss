2f34ad8a27feab874ab386e7009f5e749702a3f6
Sergey <mavericusdev@gmail.com>
2020-10-28 07:25:28 +0300

    init: start task virtual-keyboard

---
+++ virtual-keyboard/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const isProd = !isDev
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+
+const cssLoaders = extra => {
+  const loaders = [
+    {
+      loader: miniCssExtractPlugin.loader,
+      options: {
+        hmr: isDev,
+        reloadAll: true
+      },
+    },
+    'css-loader'
+  ]
+  if (extra) {
+    loaders.push(extra)
+  }
+  return loaders
+}
+
+
+module.exports = {
+  entry: './src/js/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: filename('js')
+  },
+  devtool: 'source-map',
+  devServer: {
+    port: 4200,
+    hot: isDev,
+    contentBase: './dist',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: isProd
+      }
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/assets/favicon.ico'),
+          to: path.resolve(__dirname, 'dist/favicon.ico'),
+        }
+      ]
+    }),
+    new miniCssExtractPlugin({
+      filename: filename('css')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: cssLoaders()
+      }
+    ]
+  }
+};
